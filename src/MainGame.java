cb650939a452898f922f2e50900130b30fafb10c
vdgos <dgosan37@gmail.com>
2019-12-06 16:52:30 +0200

    Upper bar and Life-counter(hearts) added. + some other little corrections

--- src/MainGame.java
+++ src/MainGame.java
@@ -10,19 +10,21 @@ public class MainGame extends GraphicsProgram {
     /**The width of the window*/
     private static final double MAP_WIDTH = 500;
     /**The height of the window*/
-    private static final double MAP_HEIGHT = 700;
+    private static final double MAP_HEIGHT = 750;
     /**The width of the platform*/
-    private static final int PLATFORM_WIDTH =100;
+    private static final double PLATFORM_WIDTH =100;
     /**The height of the platform*/
-    private static final int PLATFORM_HEIGHT =10;
+    private static final double PLATFORM_HEIGHT =10;
     /**The diameter of the ball*/
     private static final double DIAM_BALL = 15;
     /**Starting (X) coordinate for the ball and the platform*/
     private static final double X_START = MAP_WIDTH/2;
     /**Starting (Y) coordinate for platform */
-    private static final double Y_START = 600;
+    private static final double Y_START = 6*MAP_HEIGHT/7;
     /**Delay between different objects' moves*/
     private static final double DELAY = 3;
+    /**The height of the upper bar, where quantity of lives is displayed*/
+    private static final double BAR_HEIGHT = MAP_HEIGHT/15;
 
     /**speed of the ball (X)*/
     private double xVel = 0;
@@ -34,9 +36,12 @@ public class MainGame extends GraphicsProgram {
     private int timer=0;
     /**Number of users' lives*/
     private int lives = 3;
+    /**Number of bricks left*/
+    private int bricksQuantity = 100;
 
     private GOval ball;
     private GRect platform;
+    private GRect upperBar;
 
     public void init() {
         this.setSize((int)MAP_WIDTH, (int)MAP_HEIGHT);
@@ -44,11 +49,12 @@ public class MainGame extends GraphicsProgram {
         addMouseListeners();
     }
 
-    /**Arrangement of necessary objects (ball, platform, bricks)*/
+    /**Arrangement of necessary objects (ball, platform, bricks, upperBar)*/
     private void setup() {
         addBricks();
         addBall(X_START,MAP_HEIGHT/2,DIAM_BALL/2,Color.BLACK);
         addPlatform(X_START,Y_START, PLATFORM_WIDTH, PLATFORM_HEIGHT,Color.BLACK);
+        addUpperBar(0,0,MAP_WIDTH,BAR_HEIGHT,Color.LIGHT_GRAY);
         while(xVel<0.75 && xVel>-0.75){
             xVel = rgen.nextDouble(-2, 2);
         }
@@ -57,22 +63,25 @@ public class MainGame extends GraphicsProgram {
     public void run(){
         setup();
         while(x==0){pause(1);} // while mouse is out of window, program sleeps
-        while(lives > 0) {
+        while(lives > 0 && bricksQuantity>0) {
             if(timer > 0) timer--;
             moveBall();
             checkForCollision();
             pause(DELAY);
         }
-
+        System.exit(0); // temporary solution
     }
+    /**This method changes (X) coordinate of the platform as the mouse moves*/
     public void mouseMoved(MouseEvent e){
-        platform.setLocation(e.getX()- PLATFORM_WIDTH /2,Y_START);
-        x=e.getX();
+        if(!(e.getX() + PLATFORM_WIDTH /2>MAP_WIDTH || e.getX() - PLATFORM_WIDTH /2<0)){
+            platform.setLocation(e.getX()- PLATFORM_WIDTH /2,Y_START);
+            x=e.getX();
+        }
     }
 
     /**This method creates the bricks for this game*/
-    public void addBricks(){
-        for(double i = MAP_HEIGHT/10; i < MAP_HEIGHT/10+0.2*MAP_HEIGHT; i += 0.02*MAP_HEIGHT){
+    private void addBricks(){
+        for(double i = MAP_HEIGHT/10 + 50; i < MAP_HEIGHT/10+0.2*MAP_HEIGHT + 50; i += 0.02*MAP_HEIGHT){
             Color color = rgen.nextColor();
             for(double j = MAP_WIDTH*0.01; j < MAP_WIDTH; j += 0.099*MAP_WIDTH){
                 GRect brick = new GRect(j,i,0.089*MAP_WIDTH,0.015*MAP_HEIGHT);
@@ -90,12 +99,15 @@ public class MainGame extends GraphicsProgram {
 
     /** This method checks for collisions*/
     private void checkForCollision() {
-        if(ball.getY()<DIAM_BALL/2){  //for ceiling
+        if(ball.getY()<=Math.abs(yVel)+BAR_HEIGHT){  //for ceiling
             yVel = -yVel;
-        } else if((ball.getX()>getWidth()-DIAM_BALL)||(ball.getX()<DIAM_BALL/2)){  //for walls
+        }
+        if((ball.getX()>=MAP_WIDTH-DIAM_BALL)||(ball.getX()<=-Math.abs(yVel))){  //for walls
             xVel = -xVel;
-        } else if(ball.getY()-5>MAP_HEIGHT){
+        }
+        if(ball.getY()-5>MAP_HEIGHT){
             lives--;
+            minusLife();
             if(lives>0) {
                 ball.setLocation(platform.getX() + PLATFORM_WIDTH / 2, platform.getY() - DIAM_BALL * 2);
                 yVel = -yVel;
@@ -129,6 +141,7 @@ public class MainGame extends GraphicsProgram {
     private boolean bricksRemover(double xCoord, double yCoord, int num){
         if(getElementAt(xCoord, yCoord) != null && getElementAt(xCoord, yCoord) != platform){
             remove(getElementAt(xCoord, yCoord));
+            bricksQuantity--;
             return true;
         } else if(getElementAt(xCoord, yCoord) == platform && timer==0){
             if(xVel*num>0){
@@ -154,6 +167,32 @@ public class MainGame extends GraphicsProgram {
         ball.setFilled(true);
         ball.setColor(color);
         add(ball);
+    }
+
+    /**This method adds upperBar and all its components (hearts, )*/
+    private void addUpperBar(double x, double y, double w,double h, Color color){
+        upperBar = new GRect(x,y,w,h);
+        upperBar.setFilled(true);
+        upperBar.setFillColor(color);
+        upperBar.setColor(color);
+        add(upperBar);
+        add(new GImage("Heart.png"),MAP_WIDTH-129,7*BAR_HEIGHT/50);
+        add(new GImage("Heart.png"),MAP_WIDTH-87,7*BAR_HEIGHT/50);
+        add(new GImage("Heart.png"),MAP_WIDTH-45,7*BAR_HEIGHT/50);
+    }
+
+    /**This method changes a red heart for a white heart, when users lose the ball*/
+    private void minusLife(){
+        if(lives==2){
+            remove(getElementAt(MAP_WIDTH-25,BAR_HEIGHT/2));
+            add(new GImage("Heart_empty.png"),MAP_WIDTH-45,7*BAR_HEIGHT/50);
+        } else if(lives==1){
+            remove(getElementAt(MAP_WIDTH-67,BAR_HEIGHT/2));
+            add(new GImage("Heart_empty.png"),MAP_WIDTH-87,7*BAR_HEIGHT/50);
+        } else if (lives == 0){
+            remove(getElementAt(MAP_WIDTH-109,BAR_HEIGHT/2));
+            add(new GImage("Heart_empty.png"),MAP_WIDTH-129,7*BAR_HEIGHT/50);
+        }
     }
 
 }
